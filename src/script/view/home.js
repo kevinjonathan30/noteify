81f71173075c51df56ded2374db0710fe3348b63
KevinJonathan30 <kevinjonathan3010@gmail.com>
2024-07-16 16:36:35 +0900

    Add API call for insert and fetch

--- src/script/view/home.js
+++ src/script/view/home.js
@@ -2,6 +2,7 @@ import Utils from '../utils.js';
 import Notes from '../data/notes.js';
 
 const home = () => {
+    const baseUrl = "https://notes-api.dicoding.dev/v2/notes";
     const searchFormElement = document.querySelector('search-bar');
     const noteListContainerElement = document.querySelector(".card-list");
     const noteModalElement = document.querySelector('note-modal');
@@ -23,6 +24,7 @@ const home = () => {
             const noteItemElement = document.createElement('noteify-card');
             noteItemElement.setAttribute('title', note.title);
             noteItemElement.setAttribute('body', note.body);
+
             noteListContainerElement.appendChild(noteItemElement);
         });
     };
@@ -30,19 +32,60 @@ const home = () => {
     const onNoteAdded = (event) => {
         const { title, body } = event.detail;
 
-        const newNote = {
+        let note = {
             title: title,
-            body: body,
+            body: body
         };
 
-        Notes.insertNote(newNote);
+        insertNote(note);
+    };
 
-        showCard("");
+    // TODO: Fix duplicate
+    async function getNotes() {
+        try {
+            let response = await fetch(baseUrl);
+
+            let responseJson = await response.json();
+            let notes = responseJson.data;
+
+            for (const note of notes) {
+                Notes.insertNote(
+                    note.id,
+                    note.title,
+                    note.body,
+                    note.archived,
+                    note.createdAt
+                );
+            }
+
+            showCard("");
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async function insertNote(note) {
+        try {
+            const options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(note)
+            };
+
+            const response = await fetch(baseUrl, options);
+            const responseJson = await response.json();
+            console.log(responseJson.message);
+            getNotes();
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     searchFormElement.addEventListener('submit', onSearchHandler);
     noteModalElement.addEventListener('note-added', onNoteAdded);
-    showCard("");
+    getNotes();
 };
 
 export default home;
